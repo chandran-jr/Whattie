55058c1a804f1d74431c054dd674d15ebc955712
Govind B Chandran <govindchandran235@gmail.com>
2021-04-25 01:45:24 +0530

    Add files via upload

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,15 +2,19 @@ import React from 'react';
 import {Tab, Nav} from 'react-bootstrap';
 
 export default function Sidebar({id}) {
+
+    const CONVERSATIONS_KEY = "conversations";
+    const CONTACTS_KEY = "contacts";
+
     return (
         <div style = {{width: '250px'}} className="d-flex flex-column">
            <Tab.Container>
-                <Nav>
+                <Nav variant="tabs" className="justify-content-center">
                     <Nav.Item>
-                        <Nav.Link eventKey="conversations">Conversations</Nav.Link>
+                        <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="contacts">Contacts</Nav.Link>
+                        <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
                     </Nav.Item>
                 </Nav>
            </Tab.Container> 
