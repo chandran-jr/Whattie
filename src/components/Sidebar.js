329b29fe95602ee2f863ab10abdbe64fa13a7ed0
Govind B Chandran <govindchandran235@gmail.com>
2021-04-25 01:40:52 +0530

    Add files via upload

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,8 +3,17 @@ import {Tab, Nav} from 'react-bootstrap';
 
 export default function Sidebar({id}) {
     return (
-        <div>
-           <Tab.Container/> 
+        <div style = {{width: '250px'}} className="d-flex flex-column">
+           <Tab.Container>
+                <Nav>
+                    <Nav.Item>
+                        <Nav.Link eventKey="conversations">Conversations</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="contacts">Contacts</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+           </Tab.Container> 
         </div>
     )
 }
