030953b730bd227b2739c62cdec4862c6749a09e
Govind B Chandran <govindchandran235@gmail.com>
2021-04-24 21:25:46 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Login from './Login';
 function App() {
   return (
-   "Hello"
+   <Login/>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function Login() {
+    return (
+        <div>
+            <Container>
+                Hello 
+            </Container>
+        </div>
+    )
+}
