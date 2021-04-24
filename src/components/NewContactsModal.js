b55e1f80f00371954a4559c0b04b41256b911f45
Govind B Chandran <govindchandran235@gmail.com>
2021-04-25 02:24:13 +0530

    Add files via upload

--- src/components/NewContactsModal.js
+++ src/components/NewContactsModal.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { Modal } from 'react-bootstrap'
+import { Modal, Form, Button } from 'react-bootstrap'
 
 
 export default function NewContactsModal({closeModal}) {
+
+    function handleSubmit(e) {
+        e.preventDefault()
+    }
+
     return (
         <div>
-            <Modal.Header>
+            <Modal.Header closeButton>
                 Create a contact
             </Modal.Header>
+            <Modal.Body onSubmit={handleSubmit}>
+                <Form>
+
+                </Form>
+            </Modal.Body>
         </div>
     )
 }
