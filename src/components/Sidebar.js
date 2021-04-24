1c3223835e13b4abaeb25f00370f2e83b9c33ea8
Govind B Chandran <govindchandran235@gmail.com>
2021-04-25 01:49:16 +0530

    Add files via upload

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Tab, Nav} from 'react-bootstrap';
 
 export default function Sidebar({id}) {
 
     const CONVERSATIONS_KEY = "conversations";
     const CONTACTS_KEY = "contacts";
+    const [activeKey,setActiveKey] = useState(CONVERSATIONS_KEY);
 
     return (
         <div style = {{width: '250px'}} className="d-flex flex-column">
-           <Tab.Container>
+           <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
                 <Nav variant="tabs" className="justify-content-center">
                     <Nav.Item>
                         <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
