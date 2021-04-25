7f95483df011126225ab996a8be7a169a3dbe4b8
Govind B Chandran <govindchandran235@gmail.com>
2021-04-25 11:58:09 +0530

    Add files via upload

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {ListGroup} from 'react-bootstrap';
+import {useContacts} from '../contexts/ContactsProvider';
 
 export default function Contacts() {
+
+    const {contacts} =  useContacts();
+
     return (
-        <div>
-            
-        </div>
+        <ListGroup variant="flush">
+            {contacts.map(contact => (
+                <ListGroup.Item key={contact.id}>
+                    {contact.name}
+                </ListGroup.Item>
+            )
+            )
+            }
+        </ListGroup>
     )
 }
 
