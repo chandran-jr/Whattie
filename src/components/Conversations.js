75e903988286bddc9048c75e3e0d8f73e1563ba1
Govind B Chandran <govindchandran235@gmail.com>
2021-04-25 15:22:32 +0530

    Add files via upload

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {ListGroup} from 'react-bootstrap';
+import { useConversations } from '../contexts/ConversationsProvider';
 
 export default function Conversations() {
+
+    const {conversations} = useConversations()
+
     return (
         <div>
-            
+            <ListGroup variant="flush">
+            {conversations.map((conversation,index) => (
+                <ListGroup.Item 
+                key={index}
+                >
+                    {conversation.recipients.map(r => r.name).join(', ')}
+                </ListGroup.Item>
+            )
+            )
+            }
+        </ListGroup>
         </div>
     )
 }
