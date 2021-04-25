64052f9901fcf9537149d059cba6639c41748c36
Govind B Chandran <govindchandran235@gmail.com>
2021-04-25 16:13:57 +0530

    Add files via upload

--- src/components/OpenConversation.js
+++ src/components/OpenConversation.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Form, InputGroup} from 'react-bootstrap';
 
 export default function OpenConversation() {
+
+    const [text,setText] = useState("");
+
+
+
     return (
         <div className="d-flex flex-column flex-grow-1">
 
         <div className="flex-grow-1 overflow-auto">
 
         </div>
-            
+            <Form>
+
+                <Form.Group>
+                    <InputGroup>
+                        <Form.Control 
+                        as="textarea" 
+                        required 
+                        onChange={e => setText(e.target.value)}
+                        value={text}
+
+                        />
+
+                    </InputGroup>
+                </Form.Group>
+
+            </Form>
         </div>
     )
 }
