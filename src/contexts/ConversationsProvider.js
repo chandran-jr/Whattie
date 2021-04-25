14d3444a282a68d7550b82d9fed29f6963f7fc8c
Govind B Chandran <govindchandran235@gmail.com>
2021-04-25 12:46:51 +0530

    Add files via upload

--- src/contexts/ConversationsProvider.js
+++ src/contexts/ConversationsProvider.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
 import useLocalStorage from '../hooks/useLocalStorage';
+import { useContacts } from './ContactsProvider';
 
 const ConversationsContext = React.createContext();
 
@@ -11,14 +12,32 @@ export function ConversationsProvider({children}) {
 
     const [conversations,setConversations] = useLocalStorage('conversations', []);
 
+    const {contacts} = useContacts();
+
     function createConversation(recipients) {
         setConversations(prevConversations => {
             return [...prevConversations, {recipients, message: []}]
         })
     }
 
+    const formattedConversations = conversations.map(conversation => {
+        const recipients = conversation.recipients.map(recipient => {
+            const contact = contacts.find(contact => {
+                return contact.id === recipient
+            })
+            const name = (contact && contact.name) || recipient
+            return {id: recipient, name}
+        })
+        return {...conversation, recipients}
+    })
+
+    const value = {
+        conversations: formattedConversations,
+        createConversation
+    }
+
     return (
-        <ConversationsContext.Provider value={{conversations, createConversation}}>
+        <ConversationsContext.Provider value={value}>
             {children}
         </ConversationsContext.Provider>
     )
