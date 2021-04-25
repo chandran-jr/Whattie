06d612566b2665f16ecec7029e108f0f85f95d19
Govind B Chandran <govindchandran235@gmail.com>
2021-04-25 16:10:11 +0530

    Add files via upload

--- src/components/OpenConversation.js
+++ src/components/OpenConversation.js
@@ -3,6 +3,10 @@ import React from 'react';
 export default function OpenConversation() {
     return (
         <div className="d-flex flex-column flex-grow-1">
+
+        <div className="flex-grow-1 overflow-auto">
+
+        </div>
             
         </div>
     )
