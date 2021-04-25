4ad80c692e37890e7afc557b48979091d957d9ad
Govind B Chandran <govindchandran235@gmail.com>
2021-04-25 11:08:48 +0530

    Add files via upload

--- src/components/NewContactsModal.js
+++ src/components/NewContactsModal.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import { Modal, Form, Button } from 'react-bootstrap'
+import React, {useRef} from 'react';
+import { Modal, Form, Button } from 'react-bootstrap';
 
 
 export default function NewContactsModal({closeModal}) {
+
+    const idRef = useRef();
+    const nameRef = useRef();
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -15,7 +18,15 @@ export default function NewContactsModal({closeModal}) {
             </Modal.Header>
             <Modal.Body onSubmit={handleSubmit}>
                 <Form>
+                    <Form.Group>
+                        <Form.Label>ID</Form.Label>
+                        <Form.Control type="text" ref={idRef} required/>
+                    </Form.Group>
 
+                    <Form.Group>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" ref={nameRef} required/>
+                    </Form.Group>
                 </Form>
             </Modal.Body>
         </div>
