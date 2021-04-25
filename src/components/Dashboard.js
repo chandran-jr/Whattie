679ad208c2f251396b72afd7187eaa5d1de61f20
Govind B Chandran <govindchandran235@gmail.com>
2021-04-25 16:01:15 +0530

    Add files via upload

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import OpenConversation from './OpenConversation';
 import Sidebar from './Sidebar';
 
 export default function Dashboard({id}) {
     return (
         <div className="d-flex" style={{height: "100vh"}}>
             <Sidebar id={id}/>
+            <OpenConversation/>
         </div>
     )
 }
